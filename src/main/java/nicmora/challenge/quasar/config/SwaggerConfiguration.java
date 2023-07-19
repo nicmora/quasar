105bdf470496a5d005a227f3540614c015a68d8d
nicmora <nicolas.morales@wenance.com>
2023-07-19 16:08:15 -0300

    Added swagger openapi webflux

---
+++ src/main/java/nicmora/challenge/quasar/config/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package nicmora.challenge.quasar.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Quasar")
+                        .version("1.0.0")
+                        .description("Reactive Quasar ML Challenge"));
+    }
+
+}
